cd9b57b0671d47b4002ac86fbe48520b96bdb2a0
Tim Zeng <zengtiantian1122@gmail.com>
2017-10-23 21:03:07 -0700

    setup basic page styling

--- src/components/app.js
+++ src/components/app.js
@@ -5,8 +5,13 @@ import SortableListComponent from './sortableList';
 export default class App extends Component {
   render() {
     return (
-      <div className='container'>
-        <SortableListComponent />
+      <div>
+        <header>
+          <h3>Sortable Task List - Tim Zeng - ReactJS & Redux</h3>
+        </header>
+        <div className='container content'>
+          <SortableListComponent />
+        </div>
       </div>
     );
   }
